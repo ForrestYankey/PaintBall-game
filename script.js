485060b54bece7f30f93dd09f47ab3f2a6ea4290
Forrest Yankey <129310337+ForrestYankey@users.noreply.github.com>
2023-04-28 10:56:36 -0400

    Add firstRound() and secondRound()

--- script.js
+++ script.js
@@ -11,12 +11,44 @@ function computerPlay() {
     return computerChoice
 }
 
-console.log(computerPlay());
 //firstround functoin where player choses to shoot left, right, or middle
+function firstRound (playerSelection, computerSelection) {
 //if player choice is the same as computer choice, players gets 1 point
+    if (playerSelection === 'left' && computerSelection === 'left') {
+        playerScore++;
+        return "You shot the computer!";
+    } else if (playerSelection === 'right' && computerSelection === 'right') {
+        playerScore++;
+        return "You shot the computer!";
+    } else if (playerSelection === 'middle' && computerSelection === 'middle') {
+        playerScore++;
+        return "You shot the computer!";
+    } else {
+        return "You missed the computer."
+    }
+}
+
+const playerSelection = 'right';
+const computerSelection = computerPlay();
+
+console.log(firstRound(playerSelection, computerSelection));
 
 //second round function where the player choses to dogde left, right, or middle
 //if player choice is the same as computer choice, computer gets one point
-
+function secondRound (playerSelection, computerSelection) {
+    //if player choice is the same as computer choice, players gets 1 point
+        if (playerSelection === 'left' && computerSelection === 'left') {
+            computerScore++;
+            return "You were shot by the computer!";
+        } else if (playerSelection === 'right' && computerSelection === 'right') {
+            computerScore++;
+            return "You were shot by the computer!";
+        } else if (playerSelection === 'middle' && computerSelection === 'middle') {
+            computerScore++;
+            return "You were shot by the computer!";
+        } else {
+            return "You dodged the computers shot."
+        }
+    }
 //game function where first and second rounds repeat until player or computer has score of 3
 //first to 3 wins
